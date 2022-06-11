cc6f9d58713c20ae5b8a089b2606ce6d14e6c0a9
Nikola <nikola94lukic@hotmail.com>
2022-06-12 06:50:29 +1300

    Catalog repo & connection to mongoDB

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,50 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            // Check if existing product coll in our DB, if not, seed 
+            bool existProduct = productCollection.Find(p => true).Any();
+
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "602d4353gre",
+                    Name = "IPhone X",
+                    Summary = "IPhone Summary",
+                    Description = "Lorem ipsum bla bla",
+                    ImageFile = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "602d423423wqergre",
+                    Name = "Samsung 10",
+                    Summary = "Samsung Summary",
+                    Description = "Lorem ipsum Samsuing bla bla",
+                    ImageFile = "product-2.png",
+                    Price = 840.00M,
+                    Category = "Smart Phone"
+                },
+            }
+        }
+    }
+}
